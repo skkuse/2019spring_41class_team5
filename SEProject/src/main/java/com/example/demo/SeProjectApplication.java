a2acbfc766341d622457d3c53f55ddab177fce30
jihhh0625 <38034921+jihhh0625@users.noreply.github.com>
2019-05-20 14:53:40 +0900

    spring boot

---
+++ SEProject/src/main/java/com/example/demo/SeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeProjectApplication.class, args);
+	}
+
+}
