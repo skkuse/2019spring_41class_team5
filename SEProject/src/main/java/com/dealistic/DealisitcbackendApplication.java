a22933284adfe31ad3a9833c419311edc9d9698b
Muhammad Shakeel Zuhaib (Dahab) <48248272+goldshakil@users.noreply.github.com>
2019-05-22 02:26:28 +0900

    Added some User Beans and Controllers
    Bugs Are Still There. This is an intial Code

---
+++ SEProject/src/main/java/com/dealistic/DealisitcbackendApplication.java
@@ -0,0 +1,13 @@
+package com.dealistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealisitcbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealisitcbackendApplication.class, args);
+	}
+
+}
---
+++ SEProject/src/main/java/com/dealistic/beans/User.java
@@ -0,0 +1,26 @@
+package com.dealistic.beans;
+
+
+public class User {
+    String name;
+    int age;
+    String registrationNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+}
