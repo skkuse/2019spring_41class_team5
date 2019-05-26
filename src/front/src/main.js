937275f7338e6e366ca4e7178a8d75f68eea0d5f
Beurett <junbbang5@gmail.com>
2019-05-27 01:26:09 +0900

    Add global stylesheet, shorthand call functions

--- src/front/src/main.js
+++ src/front/src/main.js
@@ -1,7 +1,10 @@
 // Import root libraries
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Ionic from '@ionic/vue'
+import Moment from 'vue-moment'
 import Axios from 'axios'
+import Action from './util/action'
 
 // Import root components
 import App from './App.vue'
@@ -9,29 +12,33 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-// Import ionic css
+// Import css
 import '@ionic/core/css/core.css'
 import '@ionic/core/css/ionic.bundle.css'
 import './theme/variables.scss'
+import './theme/global.scss'
 
 Vue.config.productionTip = false
 
-
 // Library integration
-Vue.prototype.$http = Axios
-Ionic.install(Vue, {
-})
+Vue.use(Moment)
+Vue.use(Action)
+Vue.use(Ionic)
+
+// HTTP config
+Vue.prototype.$http = Axios.create({
+  baseURL: '/backend/'
+});
 
 // Global filters
 Vue.filter('ordinalize', function (value) {
   if (!value) return ''
-  let firstValue = value % 10
+  const firstValue = value % 10
   if (firstValue === 1) return value + 'st'
   if (firstValue === 2) return value + 'nd'
   if (firstValue === 3) return value + 'rd'
   else return value + "th"
 })
-
 Vue.filter('percent', function (value, maxValue) {
   return (value / maxValue) * 100
 })
