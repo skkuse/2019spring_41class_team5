8ba585bcbcbc3ebe5a0d3b85554671d7a5653aaa
Muhammad Shakeel Zuhaib (Dahab) <48248272+goldshakil@users.noreply.github.com>
2019-06-02 03:39:12 +0900

    Add files via upload

---
+++ src/back/src/main/java/nlp/Pipeline.java
@@ -0,0 +1,33 @@
+package nlp;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
+
+public class Pipeline {
+    private static Properties properties;
+    private static String propertiesName="tokenize,ssplit,,parse,sentiment,pos,lemma";
+    private static StanfordCoreNLP stanfordCoreNLP;
+
+    private Pipeline()
+    {
+
+    }
+
+    static
+    {
+        properties=new Properties();
+        properties.setProperty("annotators",propertiesName);
+    }
+
+    public static StanfordCoreNLP getPipeline()
+    {
+        if(stanfordCoreNLP==null)
+        {
+            stanfordCoreNLP=new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+
+    }
+}
